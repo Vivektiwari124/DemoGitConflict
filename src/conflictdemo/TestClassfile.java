21abaefd0d955ae69684ec9bc3e3619bfd5d8e66
vivek.tiwari <vivek.tiwari@Vivek-Tiwari1.diasparkindia.com>
2019-07-15 23:01:45 +0530

    added two charactar & add one print out message

--- src/conflictdemo/TestClassfile.java
+++ src/conflictdemo/TestClassfile.java
@@ -3,10 +3,11 @@ package conflictdemo;
 public class TestClassfile {
 
 	public static void main(String[] args) {
-		String[] names = { "vivek", "dsdsd", "dsdsd", "dsdsdsds", "dsdsdsdsd" };
+		String[] names = { "vivek", "dsdsd", "dsdsd", "dsdsdsds", "dsdsdsdsd","rahul","vivek" };
 
 		for (int i = 0; i < names.length; i++) {
 			System.out.println(names[i]);
+			System.out.println("what are you doing today");
 		}
 
 	}
