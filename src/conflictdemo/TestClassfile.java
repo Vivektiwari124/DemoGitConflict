3e49befcd146066e81797992006c00d7f74fed26
admin <admin@192.168.97.131>
2019-07-15 22:57:43 +0530

    change for loop & add one message

--- src/conflictdemo/TestClassfile.java
+++ src/conflictdemo/TestClassfile.java
@@ -5,8 +5,10 @@ public class TestClassfile {
 	public static void main(String[] args) {
 		String[] names = { "vivek", "dsdsd", "dsdsd", "dsdsdsds", "dsdsdsdsd" };
 
-		for (int i = 0; i < names.length; i++) {
-			System.out.println(names[i]);
+		// This is print message will display names array
+		for (String name : names) {
+			System.out.println("helloooo" + name);
+
 		}
 
 	}
