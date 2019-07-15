376dff918025f2ad89a751cb419c63091cfd9d11
admin <admin@192.168.97.131>
2019-07-15 22:22:50 +0530

    first commit changes

---
+++ src/conflictdemo/TestClassfile.java
@@ -0,0 +1,14 @@
+package conflictdemo;
+
+public class TestClassfile {
+
+	public static void main(String[] args) {
+		String[] names = { "vivek", "dsdsd", "dsdsd", "dsdsdsds", "dsdsdsdsd" };
+
+		for (int i = 0; i < names.length; i++) {
+			System.out.println(names[i]);
+		}
+
+	}
+
+}
